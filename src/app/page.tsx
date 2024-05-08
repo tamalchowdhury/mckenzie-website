12bf5b034371c4f7fad4d6de87d4adacedadda60
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2024-05-08 20:02:02 +0600

    Layout and structure in tailwind css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,107 @@
 import Image from "next/image";
+import logo from "@/img/logo.jpg"
+import Link from "next/link";
+
+const routes = [
+  {
+    name: "Home",
+    path: "/"
+  },
+  {
+    name: "Renovations",
+    path: "/renovations"
+  },
+  {
+    name: "New Homes",
+    path: "/new-homes"
+  },
+  {
+    name: "Blog",
+    path: "/blog"
+  },
+  {
+    name: "Contact",
+    path: "/contact"
+  },
+  {
+    name: "Free Quote",
+    path: "/free-quote"
+  }
+]
+
+const inputs = [
+  {
+    label: "Name",
+    type: "text",
+    id: "name",
+    placeholder: "Name"
+  },
+  {
+    label: "Email",
+    type: "email",
+    id: "email",
+    placeholder: "Email"
+  },
+  {
+    label: "Phone",
+    type: "text",
+    id: "phone",
+    placeholder: "Phone"
+  },
+  {
+    label: "Your Inquiry",
+    type: "textarea",
+    id: "message",
+    placeholder: "Message"
+  }
+]
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+    <main className="wrapper bg-white lg:w-[1024px] mx-auto p-7 mt-4">
+      <header className="header flex">
+        <div className="logo">
+        <Link href="/">
+          <Image src={logo} alt="Mckenzie and Paz Builders logo"></Image>
+        </Link>
         </div>
-      </div>
+        <nav className="nav border-b-8 border-[#841618] w-[100%] flex items-center uppercase font-[300]">
+          <ul className="flex justify-between ml-6 w-[100%]">
+            {routes.map((route, index) => (
+              <li key={index}>
+                <Link href={route.path} className="hover:text-[#841618] transition">{route.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+      <div className="content grid grid-cols-[1fr_300px] mt-6 gap-6">
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <section className="hero border-[1px] ">
+        section hero image 
+        <div className="address">95 Garden Place COSTERFIELD VIC 3523</div>
+      </section>
+      <section className="free-quote  bg-[#E1DCC6] border-[1px] border-[#C1B594] p-5">
+        <h2 className="font-bold uppercase text-xl text-[#841618] mb-4">Get A Free Quote</h2>
+        <p className="text-[14px] mb-4">Simply enter a few details below and we will get back to you within 24 hours.</p>
+        <form action="">
+          {inputs.map((input, index) => (
+            <label className="font-bold block mb-4 text-[#333]" htmlFor={input.id} key={index}>{input.label}:
+              {input.type === "textarea" ? (
+                <textarea className="w-[100%] p-1 px-2 font-normal mt-2 rounded-md" id={input.id} />
+              ) : (
+                <input className="w-[100%] p-1 px-2 font-normal mt-2 rounded-md" type={input.type} id={input.id} ></input>
+              )}
+            </label>
+          ))}       
+          <button type="submit" className="block w-full bg-[#841618] p-2 mb-2 font-bold text-white/90 hover:text-white rounded-md uppercase transition">Get a Quote</button>
+          <small className="text-xs">We respect your privacy. <a href="#" className="text-[#841618]">Privacy Policy</a></small>
+        </form>
+      </section>
       </div>
     </main>
+    <footer className="footer text-center mt-4 text-sm text-white/70">&copy; 2024 <strong>McKenzie & Paz Builders</strong> - Developed By <Link href="https://tamalchowdhury.com" className="text-[#841618]">Tamal Chowdhury</Link></footer>
+    </>
   );
 }

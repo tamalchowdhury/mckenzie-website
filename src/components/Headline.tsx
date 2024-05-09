8c6386ddd9212d613181e8cca3103e639d493015
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2024-05-09 19:55:00 +0600

    refactor: Update Headline component to use TypeScript

--- src/components/Headline.tsx
+++ src/components/Headline.tsx
@@ -1,5 +1,9 @@
 import React from "react"
 
-export default function Headline({ children }) {
+type HeadlineProps = {
+  children: React.ReactNode
+}
+
+export default function Headline({ children }: HeadlineProps) {
   return <h1 className="text-3xl mb-6 text-accent font-bold">{children}</h1>
 }

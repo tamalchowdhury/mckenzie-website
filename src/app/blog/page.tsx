aff247dc9dcb1ae574e137f15f232b87faa50294
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2024-05-09 13:41:22 +0600

    refactor: Update BlogPage to fetch and display blog posts dynamically

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,21 @@
 import Headline from "@/components/Headline"
 import React from "react"
 
-export default function BlogPage() {
-  return <div>
-    <Headline>
-      Blog
-    </Headline>
-  </div>
+export default async function BlogPage() {
+  const response = await fetch("https://dummyjson.com/posts?limit=3")
+  const data = await response.json()
+
+  return (
+    <div>
+      <Headline>Blog</Headline>
+      <div>
+        {data.posts.map((post) => (
+          <div key={post.id} className="mb-6">
+            <h2 className="text-[#841618] font-bold text-xl mb-2">{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
